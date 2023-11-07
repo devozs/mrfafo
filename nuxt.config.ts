25529c48c0511d54c9d22bc9412166778d8b39e5
devozs <oz.shemesh@datomize.com>
2023-11-07 13:51:22 +0200

    first fafo

---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  site: {
+    url: 'https://mrfafo.devozs.com',
+    name: 'Mr FAFO',
+    description: 'Mr FAFO',
+    defaultLocale: 'he-IL',
+    trailingSlash: true,
+    identity: {
+      type: 'Person'
+    },
+    twitter: '@DevOzs',
+
+  },
+  app: {
+    head: {
+      titleTemplate: '%s %separator Mr FAFO', // WA due to bug in SEO-KIT (to be removed once upgraded)
+      templateParams: {
+        separator: '•'
+        // other common separators: '·', '—', '•'
+      }
+    }
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: [
+    '@pinia/nuxt',
+    'nuxt-og-image',
+    '@nuxtseo/module',
+    'nuxt-simple-robots',
+    'nuxt-simple-sitemap'
+  ],
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate', 'storeToRefs']
+  },
+  imports: {
+    dirs: ['./stores/**', './models'],
+    // imports: [
+    //   {from: 'pinia', name: 'defineStore'}
+    // ]
+  }
+})
