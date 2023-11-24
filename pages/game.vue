8ac67e23c4378b8a38b0f26ca5b2b9738643b67f
devozs <oz.shemesh@datomize.com>
2023-11-24 23:05:22 +0200

    cover image

--- pages/game.vue
+++ pages/game.vue
@@ -2,18 +2,18 @@
 
 useHead({
   meta: [
-    { name: 'twitter:card', content: 'summary_large_image' },
+  { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: 'Mr FAFO Mole' },
     { name: 'twitter:description', content: 'Fuck Around and Find Out' },
-    { name: 'twitter:image', content: 'https://www.mrfafo.com/cover.png' },
+    { name: 'twitter:image', content: 'https://www.mrfafo.com/images/cover.png' },
     { name: 'twitter:site', content: '@devozs' },
     { name: 'twitter:creator', content: '@devozs' },
     { name: 'og:type', content: 'website' },
     { name: 'og:title', content: 'Mr FAFO Mole' },
     { name: 'og:description', content: 'Fuck Around and Find Out' },
     { name: 'og:url', content: 'https://www.mrfafo.com' },
-    { name: 'og:image', content: 'https://www.mrfafo.com/cover.png' },
-    { name: 'og:image:secure_url', content: 'https://www.mrfafo.com/cover.png' },
+    { name: 'og:image', content: 'https://www.mrfafo.com/images/cover.png' },
+    { name: 'og:image:secure_url', content: 'https://www.mrfafo.com/images/cover.png' },
   ],
   htmlAttrs: {
     lang: 'en'
